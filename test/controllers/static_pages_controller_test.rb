c5b6edce94bb1ed87fedb2f0d62225d12caccd72
sapar <sapar@meyvndigital.co.uk>
2021-07-12 18:59:04 +0300

    save

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "TwitterLike APP"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | TwitterLike APP"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | TwitterLike APP"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | TwitterLike APP"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
