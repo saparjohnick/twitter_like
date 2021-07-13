5eaedbf484162586d386b52c5bce5e86ab8c2c4e
sapar <sapar@meyvndigital.co.uk>
2021-07-13 17:03:18 +0300

    finish layout and routes

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.1.1'
 gem 'pg', '~> 0.18'
+gem 'bootstrap-sass'
+gem 'jquery-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -45,6 +47,7 @@ group :development, :test do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rspec'
+  gem 'rails-controller-testing'
 end
 
 group :test do
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
-    @base_title = "TwitterLike APP"
+    @base_title = 'TwitterLike APP'
   end
 
-  test "should get home" do
+  test 'should get home' do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select 'title', "Home | #{@base_title}"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test 'should get about' do
+    get about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
 
+  test 'should get contact' do
+    get contact_url
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
 end
