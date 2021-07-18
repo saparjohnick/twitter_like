af45c056a86bb9072d58053d112853d9adffd007
sapar <sapar@meyvndigital.co.uk>
2021-07-19 01:36:32 +0300

    fix

--- db/migrate/20210718152209_set_default_slug_to_existed_users.rb
+++
@@ -1,5 +0,0 @@
-class SetDefaultSlugToExistedUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :users, :slug, users.id 
-  end
-end
--- db/migrate/20210718220758_remove_default_slug_from_existed_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDefaultSlugFromExistedUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :users, :slug, nil
-  end
-end
