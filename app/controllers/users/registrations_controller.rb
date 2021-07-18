9d761e508fc0f3dce171fa9ca690b38c6e1d89a1
sapar <sapar@meyvndigital.co.uk>
2021-07-19 00:51:03 +0300

    fix devise sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :verify_authenticity_token, :only => :create
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  
 
   # GET /resource/sign_up
   def new
@@ -29,11 +31,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  # private
+  private
 
-  # def sign_up_params
-  #   params.require(:user).permit()
-  # end
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
   # def account_update_params
   #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -51,9 +53,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
