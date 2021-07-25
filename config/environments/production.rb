6bb356723463f46612b25242c0662480c2187031
sapar <sapar@meyvndigital.co.uk>
2021-07-25 12:33:49 +0300

    mailgun imp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "twitter_like_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  # https://devcenter.heroku.com/articles/mailgun#deploying-to-heroku
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
