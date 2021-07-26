51fa6f6f40c95ac7988be18bb23e20a0611b3cf6
sapar <sapar@meyvndigital.co.uk>
2021-07-27 01:25:24 +0300

    fix flex and aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,15 @@ CarrierWave.configure do |config|
   config.fog_credentials = { 
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET_KEY']
-   }
-   config.fog_directory    =  ENV['S3_BUCKET']
+    :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    region: 'eu-west-1'
+  }
+  config.fog_directory    =  ENV['S3_BUCKET']
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrier_wave'
+
+
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^a-zA-Z0-9\.\-_]/
+
+
 end
