c0e687060aba31e286108181e93520b8e46a8ae7
sapar <sapar@meyvndigital.co.uk>
2021-07-28 00:33:19 +0300

    try to fix aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = { 
+  # config.fog_credentials = { 
+    
+  # }
+  config.storage =  Fog::Storage.new(
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    region: 'eu-west-1'
-  }
-  config.storage = :fog
+    :region                => 'eu-west-1'
+  )
   config.fog_directory    =  ENV['S3_BUCKET']
   config.fog_public = false
   config.root = Rails.root.join('tmp')
