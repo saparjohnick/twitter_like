d1996410908f535c064bf12ad31ff75f8d46afac
sapar <sapar@meyvndigital.co.uk>
2021-07-29 00:06:17 +0300

    fix aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
     # :host                  => "s3.us-east-1.amazonaws.com/bucket",
-    # :region                => 'us-east-1'
+    :region                => 'us-east-1'
   }
   config.storage = :fog
   config.fog_use_ssl_for_aws = false
