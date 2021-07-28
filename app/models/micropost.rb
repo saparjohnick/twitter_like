79868d6c94807f857e28991094890cf0dafa0232
sapar <sapar@meyvndigital.co.uk>
2021-07-28 15:08:52 +0300

    try to fix pictures

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,6 +8,8 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 } 
   validate :picture_size
 
+  private 
+
   def picture_size
     if picture.size > 5.megabytes
       errors.add(:picture, "should be less than 5mb")
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    :host                   => "s3-eu-west-1.amazonaws.com",
+    :host                  => "s3-eu-west-1.amazonaws.com",
     :region                => 'eu-west-1'
   }
   config.storage = :fog
