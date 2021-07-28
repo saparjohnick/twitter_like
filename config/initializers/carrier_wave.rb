564931fcbbb0956a34d7a47c7ca3824b6cf404dc
sapar <sapar@meyvndigital.co.uk>
2021-07-28 23:54:39 +0300

    fix aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,15 +3,16 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    :host                  => "s3.us-east-1.amazonaws.com/",
-    :region                => 'us-east-1'
+    # :host                  => "s3.us-east-1.amazonaws.com/bucket",
+    # :region                => 'us-east-1'
   }
   config.storage = :fog
+  config.fog_use_ssl_for_aws = false
   config.fog_directory     =  ENV['S3_BUCKET']
 
   # config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrier_wave'                  
-  config.fog_public     = true                                  
+  # config.cache_dir = 'carrier_wave'
+  # config.fog_public     = true                                  
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   CarrierWave::SanitizedFile.sanitize_regexp = /[^a-zA-Z0-9\.\-_]/
 
