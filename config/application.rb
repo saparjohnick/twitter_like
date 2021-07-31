4f144e02eba8fc2ae301791dba6c7abb93e8cb0a
sapar <sapar@meyvndigital.co.uk>
2021-08-01 01:49:40 +0300

    fix typo

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module TwitterLike
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
       "<div class=\"field_with_errors control-group error\">#{html_tag}</div>".html_safe
     }
-    config.action_view.embed_authentisity_token_in_remote_forms = true
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
