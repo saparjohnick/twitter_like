1dc4b60ae6160f93843ac70f29440ce11d543204
sapar <sapar@meyvndigital.co.uk>
2021-08-02 21:03:30 +0300

    fix spec

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  let(:another_user) { create :user }
-  let(:user) { create :user, :with_relationship, followed_id: another_user.id }
+  let!(:another_user) { create :user }
+  let!(:user) { create :user, :with_relationship, followed_id: another_user.id }
   
   @relationship = Relationship.last
-  it { expect(@relationship).to be_valid }
+  it { expect(user.active_relationships.last).to be_valid }
 end
