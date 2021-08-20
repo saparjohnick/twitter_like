3886639607ea1f3a20abe46daa56163d6349e358
sapar <sapar@meyvndigital.co.uk>
2021-08-20 16:47:20 +0300

    fix sidekiq

--- app/workers/every_day_mailing_worker.rb
+++ app/workers/every_day_mailing_worker.rb
@@ -2,7 +2,7 @@ class EveryDayMailingWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    Users.each do |user|
+    Users.all.each do |user|
       UserNotifyMailer.with(user: user).notify_email.deliver_now
     end
   end
